d668011ab82ea2a5edc65b7b8536797991bfbbe4
Muhammad Iqbal Ghozy <rizalhp12345@gmail.com>
2024-07-02 20:49:08 +0700

    fix bootstrap config

--- resources/views/oee/index.blade.php
+++ resources/views/oee/index.blade.php
@@ -4,39 +4,41 @@
 <head>
     <title>OEE Dashboard</title>
     <!-- Add CSS and JS here -->
-    <link href="{{ asset('css/app.css') }}" rel="stylesheet"> <!-- Tautan CSS Bootstrap -->
+    <link href="{{ asset('css/bootstrap.css') }}" rel="stylesheet"> <!-- Tautan CSS Bootstrap -->
 </head>
 
 <body>
-    <h1>OEE Dashboard</h1>
-    <table>
-        <thead>
-            <tr>
-                <th>No</th>
-                <th>Line</th>
-                <th>Nama Line</th>
-                <th>Tgl</th>
-                <th>Shift</th>
-                <th>Item</th>
-                <th>Seq</th>
-                <th>Timestamp</th>
-            </tr>
-        </thead>
-        <tbody>
-            @foreach ($data as $datum)
+    <x-navbar></x-navbar>
+    <div class="container my-3">
+        <h1>OEE Dashboard</h1>
+        <table class="table">
+            <thead>
                 <tr>
-                    <td>{{ $datum->no }}</td>
-                    <td>{{ $datum->line }}</td>
-                    <td>{{ $datum->nama_line }}</td>
-                    <td>{{ $datum->tgl }}</td>
-                    <td>{{ $datum->shift }}</td>
-                    <td>{{ $datum->item }}</td>
-                    <td>{{ $datum->seq }}</td>
-                    <td>{{ $datum->timestamp }}</td>
+                    <th>Line</th>
+                    <th>Nama Line</th>
+                    <th>Tgl</th>
+                    <th>Shift</th>
+                    <th>Item</th>
+                    <th>Seq</th>
+                    <th>Timestamp</th>
                 </tr>
-            @endforeach
-        </tbody>
-    </table>
+            </thead>
+            <tbody>
+                @foreach ($data as $datum)
+                    <tr>
+                        <td>{{ $datum->line }}</td>
+                        <td>{{ $datum->nama_line }}</td>
+                        <td>{{ $datum->tgl }}</td>
+                        <td>{{ $datum->shift }}</td>
+                        <td>{{ $datum->item }}</td>
+                        <td>{{ $datum->seq }}</td>
+                        <td>{{ $datum->timestamp }}</td>
+                    </tr>
+                @endforeach
+            </tbody>
+        </table>
+    </div>
+    <script src="{{ asset('js/bootstrap.js') }}"></script>
 </body>
 
 </html>
